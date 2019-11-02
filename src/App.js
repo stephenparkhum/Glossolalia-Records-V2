552c1173fa584787c56a0421cfc5f59c533623c4
Stephen Parker <stephenparkhum@outlook.com>
2019-11-02 16:40:47 -0700

    Updating header nav bar to be in a fixed position.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
 // Components
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import ArtistList from './components/ArtistList/ArtistList';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Main />
-    </div>
+    <Router>
+      <div className="App">
+      <Switch>
+        <Route exact path="/">
+          <Header />
+          <Main title="NEWS"/>
+        </Route>
+        <Route path="/artists">
+          <Header />
+          <Main title="ARTISTS"/>
+        </Route>
+      </Switch>
+        
+      </div>
+    </Router>
+    
   );
 }
 
--- src/data/artists.js
+++ src/data/artists.js
@@ -1,1 +1,98 @@
+const ARTISTS = [
+    {
+        name: 'Gash',
+        genre: 'Death Metal',
+        bio: '', 
+        band_photo_url: '',
+        albums: [
+            {
+                title: 'Crossing the Lugubrious Plateau',
+                release_date: '7/4/2018',
+                cover_art: 'https://f4.bcbits.com/img/a0368303840_10.jpg',
 
+            },
+        ]
+    },
+    {
+        name: 'Grst',
+        genre: 'Black Metal',
+        bio: '', 
+        band_photo_url: '',
+        albums: [
+            {
+                title: 'Plague Seed',
+                release_date: '9/23/2014',
+                cover_art: 'https://f4.bcbits.com/img/a1372319766_10.jpg',
+
+            },
+            {
+                title: 'Fire Therein',
+                release_date: '5/1/2014',
+                cover_art: 'https://f4.bcbits.com/img/a0599029208_10.jpg',
+
+            },
+            {
+                title: 'Grst / The Will of a Million',
+                release_date: 'January 3, 2014',
+                cover_art: 'https://f4.bcbits.com/img/a3086771504_10.jpg',
+
+            },
+        ]
+    },
+    {
+        name: 'Lodge of the Empty Bed',
+        genre: 'Blackdeath',
+        bio: '', 
+        band_photo_url: '',
+        albums: [
+            {
+                title: 'Pacta Sunt Servanda',
+                release_date: '7/4/2018',
+                cover_art: 'https://f4.bcbits.com/img/a0040955700_10.jpg',
+
+            },
+        ]
+    },
+    {
+        name: 'Maestus',
+        genre: 'Blackdoom',
+        bio: '', 
+        band_photo_url: '',
+        albums: [
+            {
+                title: 'Voir Dire',
+                release_date: '3/3/2015',
+                cover_art: 'https://f4.bcbits.com/img/a3169035059_10.jpg',
+
+            },
+            {
+                title: 'Scarlet Lakes',
+                release_date: '9/10/2013',
+                cover_art: 'https://f4.bcbits.com/img/a1456921813_10.jpg',
+
+            },
+        ]
+    },
+    {
+        name: 'Sophos',
+        genre: 'Blacknoise',
+        bio: '', 
+        band_photo_url: '',
+        albums: [
+            {
+                title: 'Voir Dire',
+                release_date: '3/3/2015',
+                cover_art: 'https://f4.bcbits.com/img/a3169035059_10.jpg',
+
+            },
+            {
+                title: 'Scarlet Lakes',
+                release_date: '9/10/2013',
+                cover_art: 'https://f4.bcbits.com/img/a1456921813_10.jpg',
+
+            },
+        ]
+    },
+];
+
+export default ARTISTS;
