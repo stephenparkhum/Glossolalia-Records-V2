5a64be4bd76eb4e8184807ed4a259ae2820e013c
Stephen Parker <stephenparkhum@outlook.com>
2019-11-03 09:08:49 -0800

    Updating styling for news and recent releases

--- src/components/RecentRelease/RecentRelease.js
+++ src/components/RecentRelease/RecentRelease.js
@@ -4,7 +4,7 @@ import './RecentRelease.css';
 const RecentReleases = () => {
     return (
         <div className="RecentReleasesMain">
-            <h3>Recent Releases</h3>
+            <h3>RECENT RELEASES</h3>
             <div className="RecentReleasesList">
                 <div className="RecentRelease">
                         <div className="RecentAlbum">
