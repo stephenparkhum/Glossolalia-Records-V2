6e09c6fbfdf2e08fb3590c3040b99035757be1da
Stephen Parker <stephenparkhum@outlook.com>
2019-11-09 23:23:49 -0800

    Updating

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,16 +3,16 @@ import './Contact.css';
 
 const Contact = () => {
     return (
-        <>
+        <div className="Contact-info">
         <h3>Shipping Inquiries</h3>
         <p>
             For questions involving orders, logistics, and all else, direct your communications to:
-            glossolaliarecords@gmail.com
+            <a href="mailto:glossolaliarecords@gmail.com">glossolaliarecords@gmail.com</a>
         </p>
         <p>
-        Sign u for the glossolalia records <a href="http://eepurl.com/dACxuP">mailing list</a> to receive information about upcoming releases, distro locations, and events.
+        Sign up for the glossolalia records <a href="http://eepurl.com/dACxuP">mailing list</a> to receive information about upcoming releases, distro locations, and events.
         </p>
-        </>
+        </div>
     )
 }
 
