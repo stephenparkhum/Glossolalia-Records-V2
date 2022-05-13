6a4a9a4a0d0f082015a8a7fe9a1a2a3f32bcf65b
SP <stephenparkhum@outlook.com>
2022-05-13 12:05:54 -0700

    Testing pre-commit hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
 
 // Components
-import Header from "./components/Header/Header";
-import Main from "./components/Main/Main";
-import ArtistIndividual from "./components/ArtistIndividual/ArtistIndividual";
-import LeftSocial from "./components/LeftSocial/LeftSocial";
-import ButtonMobileMenu from "./components/BottomMobileMenu/BottomMobileMenu";
-import { artistsRef } from "./firebaseSetup";
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import ArtistIndividual from './components/ArtistIndividual/ArtistIndividual';
+import LeftSocial from './components/LeftSocial/LeftSocial';
+import ButtonMobileMenu from './components/BottomMobileMenu/BottomMobileMenu';
+import { artistsRef } from './firebaseSetup';
 
 // Google Analytics
-import ReactGA from "react-ga";
-ReactGA.initialize("UA-65527149-2");
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-65527149-2');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App = () => {
   const [artists, setArtists] = useState([]);
 
+  // Test comment for pre-commit hook
+
   useEffect(() => {
-    artistsRef.on("value", (snapshot) => {
+    artistsRef.on('value', snapshot => {
       let allArtists = [];
-      snapshot.forEach((snap) => {
+      snapshot.forEach(snap => {
         allArtists.push(snap.val());
       });
       setArtists(allArtists);
@@ -41,7 +43,7 @@ const App = () => {
           </Route>
           <Route
             path="/artists/:artist"
-            component={(props) => {
+            component={props => {
               return <ArtistIndividual name={props.match.params.artist} />;
             }}
           />
