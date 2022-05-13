d593c743c15b9fbd9b750b750f5bd023b23ea026
SP <stephenparkhum@outlook.com>
2022-05-13 12:29:45 -0700

    Update proptypes on ArtistList

--- src/components/ArtistList/ArtistList.jsx
+++ src/components/ArtistList/ArtistList.jsx
@@ -1,17 +1,22 @@
+import PropTypes from "prop-types";
 import React from "react";
 import ArtistListItem from "./ArtistListItem/ArtistListItem";
 // import ARTISTS from '../../data/artists';
 import "./ArtistList.css";
 
 const ArtistList = ({ artists }) => {
-  console.log(artists);
-  const artistListDisplay = artists.map((artist) => (
-    <div key={artist}>
-      <ArtistListItem artist={artist} />
-    </div>
-  ));
+	console.log(artists);
+	const artistListDisplay = artists.map(artist => (
+		<div key={artist}>
+			<ArtistListItem artist={artist} />
+		</div>
+	));
 
-  return <div className="ArtistListMain">{artistListDisplay}</div>;
+	return <div className="ArtistListMain">{artistListDisplay}</div>;
+};
+
+ArtistList.propTypes = {
+	artists: PropTypes.array,
 };
 
 export default ArtistList;
