8842bca0a98950d8dc49955f7d3dea7fa778355c
SP <stephenparkhum@outlook.com>
2022-07-09 07:43:10 -0700

    Eslint fixes and removing unneeded test

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyD469gBxg_335DVfbYoec7B7TVn_ectWT4",
-  authDomain: "glossolalia-records-2022.firebaseapp.com",
-  databaseURL: "https://glossolalia-records-2022-default-rtdb.firebaseio.com",
-  projectId: "glossolalia-records-2022",
-  storageBucket: "glossolalia-records-2022.appspot.com",
-  messagingSenderId: "216128353138",
-  appId: "1:216128353138:web:955035c61eef9a004463eb",
-  measurementId: "G-VVQWRVGRHW",
+	apiKey: "AIzaSyD469gBxg_335DVfbYoec7B7TVn_ectWT4",
+	authDomain: "glossolalia-records-2022.firebaseapp.com",
+	databaseURL: "https://glossolalia-records-2022-default-rtdb.firebaseio.com",
+	projectId: "glossolalia-records-2022",
+	storageBucket: "glossolalia-records-2022.appspot.com",
+	messagingSenderId: "216128353138",
+	appId: "1:216128353138:web:955035c61eef9a004463eb",
+	measurementId: "G-VVQWRVGRHW",
 };
 
 export default firebaseConfig;
