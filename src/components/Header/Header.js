a6ffdd8528238b4a997a6a7f9c452a1832b35915
SP <stephenparkhum@outlook.com>
2022-08-10 09:20:21 -0700

    Updating prettier settings and updating packages

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 
-// Child Components
-import HeaderNav from './HeaderNav/HeaderNav';
+import HeaderNav from "./HeaderNav/HeaderNav";
 
 function Header() {
-  return (
-    <div className="Header">
-        <div className="HeaderLogo">
-            <h2><a href="/">glossolalia records</a></h2>
-        </div>
-        <HeaderNav />
-    </div>
-  );
+	return (
+		<div className="Header">
+			<div className="HeaderLogo">
+				<h2>
+					<a href="/">glossolalia records</a>
+				</h2>
+			</div>
+			<HeaderNav />
+		</div>
+	);
 }
 
 export default Header;
--- src/components/LeftSocial/LeftSocial.js
+++ src/components/LeftSocial/LeftSocial.js
@@ -1,28 +1,47 @@
-import React from 'react';
-import './LeftSocial.css';
+import React from "react";
+import "./LeftSocial.css";
 
-// Icons
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faBandcamp, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faBandcamp,
+	faFacebook,
+	faInstagram,
+	faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 
 const LeftSocial = () => {
-    return (
-        <div className="leftSocial">
-            <a href="http://glossolaliarecords.bandcamp.com" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faBandcamp} size="lg"/>
-            </a>
-            <a href="https://www.facebook.com/glossolaliarecords/" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faFacebook} size="lg"/>
-            </a>
-            <a href="https://www.instagram.com/glossolalia__records/" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faInstagram} size="lg"/>
-            </a>
-            <a href="https://www.youtube.com/channel/UCK0npcbZ33IppLUiq5GFh6A" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faYoutube} size="lg"/>
-            </a>
-            
-        </div>
-    )
+	return (
+		<div className="leftSocial">
+			<a
+				href="http://glossolaliarecords.bandcamp.com"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<FontAwesomeIcon icon={faBandcamp} size="lg" />
+			</a>
+			<a
+				href="https://www.facebook.com/glossolaliarecords/"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<FontAwesomeIcon icon={faFacebook} size="lg" />
+			</a>
+			<a
+				href="https://www.instagram.com/glossolalia__records/"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<FontAwesomeIcon icon={faInstagram} size="lg" />
+			</a>
+			<a
+				href="https://www.youtube.com/channel/UCK0npcbZ33IppLUiq5GFh6A"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<FontAwesomeIcon icon={faYoutube} size="lg" />
+			</a>
+		</div>
+	);
 };
 
 export default LeftSocial;
